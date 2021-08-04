3fd0b20ce247225cd349d6194f2a7c289d064208
bacha-11 <ijazbacha14@gmail.com>
2021-08-04 13:02:12 +0500

    find loan emi 1.5

--- loan_emi.py
+++ loan_emi.py
@@ -3,7 +3,10 @@ import math
 def loan_emi(amount, duration, rate, down_payment=0):
     rate = (rate/100)*12
     loan_amount = amount - down_payment
-    emi = loan_amount * rate * ((1 + rate)** duration) / (((1 + rate)**duration) - 1)
+    try:
+        emi = loan_amount * rate * ((1 + rate)** duration) / (((1 + rate)**duration) - 1)
+    except:
+        emi = loan_amount / duration
     emi = math.ceil(emi)
     return emi
 
@@ -12,12 +15,18 @@ total_amount = 1260000
 no_of_year = 8*12
 rate_of_interest = 10
 pay_amount = 3e5
-eight_year_result = loan_emi(amount=total_amount, 
+emi1 = loan_emi(amount=total_amount, 
                             duration=no_of_year, 
                             rate=rate_of_interest, 
                             down_payment=pay_amount)
-print(eight_year_result)
+print(emi1)
 
 
-ten_year_result = loan_emi(amount=1260000, duration=10*12, rate=8)
-print(ten_year_result)
+emi2 = loan_emi(amount=1260000, duration=10*12, rate=0)
+print(emi2)
+
+
+if emi1 < emi2:
+    print('option 1 is lower EMI s{}'.format(emi1))
+else:
+    print('option 2 is lower EMI s{}'.format(emi2))
