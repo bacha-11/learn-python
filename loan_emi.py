cf2c8c5dd10602905adb2ad1c542c3821d2a2677
bacha-11 <ijazbacha14@gmail.com>
2021-08-04 11:32:02 +0500

    find loan emi 1.1

--- loan_emi.py
+++ loan_emi.py
@@ -1,5 +1,10 @@
-def loan_emi(amount):
-    emi = amount / 12
-    print('The emi is ${}'.format(emi))
+def loan_emi(amount, duration):
+    emi = amount / duration
+    return emi
 
-loan_emi(1260000)
+eight_year_result = loan_emi(1260000, 8*12)
+print(eight_year_result)
+
+
+ten_year_result = loan_emi(1260000, 10*12)
+print(ten_year_result)
