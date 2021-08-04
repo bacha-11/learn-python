496174198117aefecde8f4a7a43053ca0f9e87a4
bacha-11 <ijazbacha14@gmail.com>
2021-08-04 12:43:12 +0500

    find loan emi 1.3

--- loan_emi.py
+++ loan_emi.py
@@ -1,11 +1,15 @@
-def loan_emi(amount, duration, down_pyment=0):
-    loan_amount = amount - down_pyment
-    emi = loan_amount / duration
+import math
+
+def loan_emi(amount, duration, rate, down_payment=0):
+    rate = (rate/100)*12
+    loan_amount = amount - down_payment
+    emi = loan_amount * rate * ((1 + rate)** duration) / (((1 + rate)**duration) - 1)
+    emi = math.ceil(emi)
     return emi
 
-eight_year_result = loan_emi(1260000, 8*12, 3e5)
+eight_year_result = loan_emi(1260000, 8*12, 10, 3e5)
 print(eight_year_result)
 
 
-ten_year_result = loan_emi(1260000, 10*12)
+ten_year_result = loan_emi(1260000, 10*12, 8)
 print(ten_year_result)
