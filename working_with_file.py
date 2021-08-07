ef6a7cea56b5ce2e69b3d6fe8146efc496247f59
bacha-11 <ijazbacha14@gmail.com>
2021-08-07 14:13:17 +0500

    working with file

---
+++ working_with_file.py
@@ -0,0 +1,26 @@
+import os
+from urllib.request import urlretrieve
+
+path = os.getcwd()
+print(path)
+
+files = os.listdir('.')
+
+print(files)
+
+
+# os.makedirs('./data', exist_ok=True)
+
+
+url1 = 'https://gist.githubusercontent.com/aakashns/257f6e6c8719c17d0e498ea287d1a386/raw/7def9ef4234ddf0bc82f855ad67dac8b971852ef/loans1.txt'
+url2 = 'https://gist.githubusercontent.com/aakashns/257f6e6c8719c17d0e498ea287d1a386/raw/7def9ef4234ddf0bc82f855ad67dac8b971852ef/loans2.txt'
+url3 = 'https://gist.githubusercontent.com/aakashns/257f6e6c8719c17d0e498ea287d1a386/raw/7def9ef4234ddf0bc82f855ad67dac8b971852ef/loans3.txt'
+
+
+# urlretrieve(url1, './data/loan1.txt')
+# urlretrieve(url2, './data/loan2.txt')
+# urlretrieve(url3, './data/loan6.txt')
+
+
+
+
