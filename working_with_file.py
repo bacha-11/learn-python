876215991d04a1ae78e18c5c8bf61625f618c997
bacha-11 <ijazbacha14@gmail.com>
2021-08-10 11:24:08 +0500

    Processing data from files

--- working_with_file.py
+++ working_with_file.py
@@ -1,44 +1,57 @@
 import os
 from urllib.request import urlretrieve
 
-path = os.getcwd()
-print(path)
+# path = os.getcwd()
+# print(path)
 
-files = os.listdir('.')
+# files = os.listdir('.')
 
-print(files)
-
-
-# os.makedirs('./data', exist_ok=True)
+# print(files)
 
 
-url1 = 'https://gist.githubusercontent.com/aakashns/257f6e6c8719c17d0e498ea287d1a386/raw/7def9ef4234ddf0bc82f855ad67dac8b971852ef/loans1.txt'
-url2 = 'https://gist.githubusercontent.com/aakashns/257f6e6c8719c17d0e498ea287d1a386/raw/7def9ef4234ddf0bc82f855ad67dac8b971852ef/loans2.txt'
-url3 = 'https://gist.githubusercontent.com/aakashns/257f6e6c8719c17d0e498ea287d1a386/raw/7def9ef4234ddf0bc82f855ad67dac8b971852ef/loans3.txt'
+# # os.makedirs('./data', exist_ok=True)
 
 
-# urlretrieve(url1, './data/loan1.txt')
-# urlretrieve(url2, './data/loan2.txt')
-# urlretrieve(url3, './data/loan6.txt')
+# url1 = 'https://gist.githubusercontent.com/aakashns/257f6e6c8719c17d0e498ea287d1a386/raw/7def9ef4234ddf0bc82f855ad67dac8b971852ef/loans1.txt'
+# url2 = 'https://gist.githubusercontent.com/aakashns/257f6e6c8719c17d0e498ea287d1a386/raw/7def9ef4234ddf0bc82f855ad67dac8b971852ef/loans2.txt'
+# url3 = 'https://gist.githubusercontent.com/aakashns/257f6e6c8719c17d0e498ea287d1a386/raw/7def9ef4234ddf0bc82f855ad67dac8b971852ef/loans3.txt'
 
 
-# how to open and close files
-loan_file1 = open('./data/loan1.txt', mode='r')
-loan_file1_content = loan_file1.read()
-print(loan_file1_content)
-loan_file1.close()
+# # urlretrieve(url1, './data/loan1.txt')
+# # urlretrieve(url2, './data/loan2.txt')
+# # urlretrieve(url3, './data/loan6.txt')
 
-print('******************************')
 
-#closing file automatically using (with)
-with open('./data/loan2.txt') as loan_file2:
-    loan_file2_content = loan_file2.read()
-    print(loan_file2_content)
+# # how to open and close files
+# loan_file1 = open('./data/loan1.txt', mode='r')
+# loan_file1_content = loan_file1.read()
+# print(loan_file1_content)
+# loan_file1.close()
 
-print("*******************************")
+# print('******************************')
 
-# read lines in files
-with open('./data/loan3.txt', 'r') as loan_file3:
-    loan_file3_content = loan_file3.readlines()
-    print(loan_file3_content)
+# #closing file automatically using (with)
+# with open('./data/loan2.txt') as loan_file2:
+#     loan_file2_content = loan_file2.read()
+#     print(loan_file2_content)
 
+# print("*******************************")
+
+# # read lines in files
+# with open('./data/loan3.txt', 'r') as loan_file3:
+#     loan_file3_content = loan_file3.readlines()
+#     print(loan_file3_content)
+
+
+
+with open('./data/loan1.txt', 'r' ) as loan_file:
+    file_data = loan_file.readlines()
+
+
+def parse_header(file):
+    return file.strip().split()
+
+
+
+header = parse_header(file_data[0])
+print(header)
