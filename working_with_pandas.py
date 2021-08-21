31b8e38c4d7bb3963cb02f58c5380d5402f4811a
bacha-11 <ijazbacha14@gmail.com>
2021-08-21 11:23:28 +0500

    working with pandas

---
+++ working_with_pandas.py
@@ -0,0 +1,5 @@
+from urllib.request import urlretrieve
+
+#italy_covid_url = 'https://gist.githubusercontent.com/aakashns/f6a004fa20c84fec53262f9a8bfee775/raw/f309558b1cf5103424cef58e2ecb8704dcd4d74c/italy-covid-daywise.csv'
+
+#urlretrieve(italy_covid_url, './data/italy-covid-daywise.csv')
